977a21b2f0e74110ab433489e0bfe32c0f885967
Jiaao Hou <jiaao.hou@yale.edu>
2015-01-07 00:52:57 -0500

    ability to view and create sets

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express'),
+		bodyParser = require('body-parser');
+
+var app = express();
+
+app.use( bodyParser.json() ); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true
+}));
+app.set('view engine', 'ejs');
+
+app.listen(3000, function() {
+	console.log('Listening on port 3000')
+})
+
+var sets = new Object();
+
+app.get('/', function(req, res) {
+	res.send('This is the root page.');
+})
+
+app.route('/sets')
+.get(function(req, res) {
+	set_names = Object.keys(sets)
+
+	res.render('sets', {sets: sets, set_names: set_names});
+})
+.post(function(req, res) {
+	set_name = req.body.set_name
+	link_list = req.body.link_list.split(", ")
+	sets[set_name] = link_list;
+	res.redirect('/sets');
+})
+
+
+app.get('/sets/new', function(req, res) {
+	res.render('new');
+})
